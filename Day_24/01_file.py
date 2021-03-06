a24705bb6faebee59589276cf1269b952d04e855
shahramDBI <shahramdarbandi79@gmail.com>
2021-03-06 09:36:44 +0330

    24

---
+++ Day_24/01_file.py
@@ -0,0 +1,16 @@
+# Read ###############################
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+######################################
+# Write ##############################
+# with open("my_file.txt", mode="w") as file:
+#     file.write("New text")
+#######################################
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew text")
+######################################
+# Create new file ####################
+# with open("new_file.txt", mode="w") as file:
+#     file.write("New text file")
+#####################################
